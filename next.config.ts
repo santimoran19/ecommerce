ec735b8c20e1e1a6b323c67d012934eef6c98ed6
santimoran19 <santimorann19@gmail.com>
2026-04-20 09:56:12 -0300

    Se mejoró la funcionalidad de la API de MP

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["postgres", "nodemailer"],
+  serverExternalPackages: ["postgres", "nodemailer", "mercadopago"],
   allowedDevOrigins: ["172.20.10.2"],
 };
 
